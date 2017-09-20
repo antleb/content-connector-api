f466f2c08c762a64ec845f23c1583aa93504e8b1
kgiann78 <kgiann78@gmail.com>
2017-09-20 16:45:30 +0300

    Code refactor: * create module content-connector-utils for common code * move heavy functionality of SearchResult (i.e. merge) to content-connector-utils * move language and faceting to content-connector-utils

--- src/main/java/eu/openminted/content/connector/SearchResult.java
+++ src/main/java/eu/openminted/content/connector/SearchResult.java
@@ -1,12 +1,9 @@
 package eu.openminted.content.connector;
 
-import eu.openminted.content.connector.faceting.OMTDFacetEnum;
 import eu.openminted.registry.core.domain.Facet;
-import eu.openminted.registry.core.domain.Value;
-import org.apache.commons.lang.WordUtils;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchResult {
 
@@ -35,197 +32,24 @@ public class SearchResult {
         this.facets = facets;
     }
 
-
-    public SearchResult merge(SearchResult result) {
-        this.setTotalHits(this.getTotalHits() + result.getTotalHits())
-                .setFacets(mergeFacets(this.getFacets(), result.getFacets()));
-
-        //TODO: how to merge results? do we need to?
-
-        return this;
-    }
-
-    public SearchResult setTotalHits(int totalHits) {
+    public void setTotalHits(int totalHits) {
         this.totalHits = totalHits;
-        return this;
     }
 
-    public SearchResult setPublications(List<String> publications) {
+    public void setPublications(List<String> publications) {
         this.publications = publications;
-        return this;
     }
 
-    public SearchResult setFrom(int from) {
+    public void setFrom(int from) {
         this.from = from;
-        return this;
     }
 
-    public SearchResult setTo(int to) {
+    public void setTo(int to) {
         this.to = to;
-        return this;
     }
 
-    public SearchResult setFacets(List<Facet> facets) {
+    public void setFacets(List<Facet> facets) {
         this.facets = facets;
-        return this;
-    }
-
-    private List<Facet> mergeFacets(List<Facet> f1, List<Facet> f2) {
-//        Map<String, Facet> mf1 = new HashMap<>();
-        if (f1 != null && f2 != null)
-            return mergeFacets(
-                    f1.stream().collect(Collectors.toMap(f->WordUtils.capitalizeFully(f.getField()), f -> f)),
-                    f2.stream().collect(Collectors.toMap(f->WordUtils.capitalizeFully(f.getField()), f -> f)))
-                    .values().stream().collect(Collectors.toList());
-
-        if (f1 == null && f2 == null) return new ArrayList<>();
-
-        if (f1 == null) return f2;
-        else return f1;
-    }
-
-    private Map<String, Facet> mergeFacets(Map<String, Facet> f1, Map<String, Facet> f2) {
-
-        Map<String, Facet> temp = new HashMap<>();
-
-        for (Map.Entry<String, Facet> e : f1.entrySet()) {
-            Facet facet;
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-
-            if (e.getKey().equalsIgnoreCase(OMTDFacetEnum.PUBLICATION_YEAR.value())) {
-                facet = buildFacet(getPublicationYearFacet(e.getValue()));
-            } else if (e.getKey().equalsIgnoreCase(OMTDFacetEnum.DOCUMENT_LANG.value())) {
-                facet = buildFacet(getLanguageFacet(e.getValue()));
-            } else
-                facet = buildFacet(e.getValue());
-
-            for (Value value : facet.getValues()) {
-                value.setValue(WordUtils.capitalizeFully(value.getValue()));
-            }
-
-            temp.put(keyName, facet);
-        }
-
-        for (Map.Entry<String, Facet> e : f2.entrySet()) {
-            Facet facet;
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-
-            if (e.getKey().equalsIgnoreCase(OMTDFacetEnum.PUBLICATION_YEAR.value())) {
-                facet = buildFacet(getPublicationYearFacet(e.getValue()));
-            } else if (e.getKey().equalsIgnoreCase(OMTDFacetEnum.DOCUMENT_LANG.value())) {
-                facet = buildFacet(getLanguageFacet(e.getValue()));
-            }
-            else {
-                facet = buildFacet(e.getValue());
-            }
-
-            for (Value value : facet.getValues()) {
-                value.setValue(WordUtils.capitalizeFully(value.getValue()));
-            }
-
-            if (temp.containsKey(e.getKey())) {
-                temp.put(keyName, mergeFacet(temp.get(keyName), facet));
-            } else {
-                temp.put(keyName, facet);
-            }
-
-            Collections.sort(temp.get(keyName).getValues());
-            Collections.reverse(temp.get(keyName).getValues());
-        }
-
-        return temp;
-    }
-
-    private Facet mergeFacet(Facet f1, Facet f2) {
-        Facet f = new Facet();
-        Map<String, Integer> temp = new HashMap<>();
-
-        f.setField(f1.getField());
-        f.setLabel(f1.getLabel());
-        f.setValues(new ArrayList<>());
-
-        for (Value v : f1.getValues()) {
-            String valueName = WordUtils.capitalizeFully(v.getValue());
-            temp.put(valueName, v.getCount());
-        }
-        for (Value v : f2.getValues()) {
-            String valueName = WordUtils.capitalizeFully(v.getValue());
-            if (temp.containsKey(valueName))
-                temp.put(valueName, v.getCount() + temp.get(WordUtils.capitalizeFully(v.getValue())));
-            else
-                temp.put(valueName, v.getCount());
-        }
-
-        for (Map.Entry<String, Integer> e : temp.entrySet()) {
-            Value v = new Value();
-            String keyName = WordUtils.capitalizeFully(e.getKey());
-
-            v.setValue(keyName);
-            v.setCount(e.getValue());
-
-            f.getValues().add(v);
-        }
-
-        return f;
-    }
-
-    private Facet getPublicationYearFacet(Facet publicationYearFacet) {
-        for (Value value : publicationYearFacet.getValues()) {
-            value.setValue(value.getValue().substring(0, 4));
-        }
-        return publicationYearFacet;
-    }
-
-
-    private Facet getLanguageFacet(Facet languageFacet) {
-
-        // Check this hashMap to convert ISO 639-2 to omtd ISO 639-1 (or ISO 639-3)
-        for (Value value : languageFacet.getValues()) {
-            if (value.getCount() > 0
-                    && LanguageConverter.getInstance().getOpenaireToOMTDName().containsKey(value.getValue())) {
-                value.setValue(LanguageConverter.getInstance().getOpenaireToOMTDName().get(value.getValue()));
-            }
-        }
-        return languageFacet;
-    }
-
-    private Facet buildFacet(Facet facet) {
-        facet.setValues(mergeValues(facet));
-        return facet;
-    }
-
-    private List<Value> mergeValues(Facet facet) {
-        Map<String, Value> tmpValues = new HashMap<>();
-
-        for (Value value : facet.getValues()) {
-            Value tmpValue = new Value();
-            tmpValue.setCount(value.getCount());
-            tmpValue.setValue(value.getValue());
-
-            String keyName = WordUtils.capitalizeFully(tmpValue.getValue());
-
-            if (tmpValues.containsKey(keyName)) {
-                int currentCount = tmpValues.get(keyName).getCount();
-                tmpValues.get(keyName).setCount(currentCount + tmpValue.getCount());
-            } else {
-                tmpValues.put(keyName, tmpValue);
-            }
-        }
-
-        List<Value> values = new ArrayList<>(tmpValues.values());
-        Collections.sort(values);
-        Collections.reverse(values);
-        return values;
-    }
-
-    public void printFacet(Facet facet) {
-        System.out.println("field:" + facet.getField());
-        System.out.println("values:[");
-        for (Value value : facet.getValues()) {
-            System.out.println(value.getValue());
-            System.out.println(value.getCount());
-        }
-        System.out.println("]");
     }
 
     public List<String> getPublications() {
